a786df8bfdbec70ed9cc4b8a8dade0f85694a8e7
Iraklis Kotsi <37937820+IraklisKts@users.noreply.github.com>
2020-06-02 10:06:07 +0300

    Create version.php

---
+++ version.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Version details.
+ *
+ * @package    local_moodlecheck
+ * @copyright  2012 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version  = 2012011000;
+$plugin->requires = 2010112400;
+$plugin->component = 'local_moodlecheck';
